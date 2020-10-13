dc42ed6021a347448c6196ca2f981b347180d409
Anil Kumar Myla <manil@verizonmedia.com>
2020-10-12 20:01:03 -0700

    Use reflection, typetag and asInstanceOf to get a working version on 1.4.0

--- src/main/scala-sbt-1.0/explicitdeps/package.scala
+++ src/main/scala-sbt-1.0/explicitdeps/package.scala
@@ -1,3 +1,5 @@
+import scala.reflect.api.JavaUniverse
+
 package object explicitdeps {
 
   type Binary = sbt.librarymanagement.Binary
@@ -6,15 +8,32 @@ package object explicitdeps {
 
   val defaultModuleFilter: ModuleFilter = sbt.librarymanagement.DependencyFilter.moduleFilter()
 
+  private val ru: JavaUniverse = scala.reflect.runtime.universe
+  private val rm: ru.Mirror = ru.runtimeMirror(getClass.getClassLoader)
+
+  private def getTypeTag[T: ru.TypeTag](obj: T) = ru.typeTag[T]
+
+  private def toFile(x: AnyRef, typeTag: String, csrCacheDirectoryValueOpt: Option[String]): java.io.File = {
+    typeTag match {
+      // sbt 1.4.0 or newer
+      case "xsbti.VirtualFileRef" =>
+        // TODO: Use reflection to invoke id() function
+        val path = x.asInstanceOf[xsbti.VirtualFile].id().replaceAllLiterally("${CSR_CACHE}", csrCacheDirectoryValueOpt.mkString)
+        new java.io.File(path)
+      // sbt 1.3.x or older
+      case "java.io.File" =>
+        x.asInstanceOf[java.io.File]
+    }
+  }
+
   def getAllLibraryDeps(analysis: Analysis, log: sbt.util.Logger)(csrCacheDirectoryValueOpt: Option[String]): Set[java.io.File] = {
     log.debug(
       s"Source to library relations:\n${analysis.relations.libraryDep.all.map(r => s"  ${r._1} -> ${r._2}").mkString("\n")}"
     )
-    val allLibraryDeps = analysis.relations.allLibraryDeps
-      .map(_.id())
-      .map(_.replaceAllLiterally("${CSR_CACHE}", csrCacheDirectoryValueOpt.mkString))
-      .map(path => new java.io.File(path))
-      .toSet
+    val allLibraryDeps = analysis.relations.allLibraryDeps.map { x =>
+      val typeTag = getTypeTag(x).tpe.toString
+      toFile(x, typeTag, csrCacheDirectoryValueOpt)
+    }.toSet
     log.debug(s"Library dependencies:\n${allLibraryDeps.mkString("  ", "\n  ", "")}")
     allLibraryDeps
   }
